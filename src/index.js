505a0244bc342d502104810cff87a02681410653
Kosi Akporji <kosiazom@gmail.com>
2020-07-14 18:03:38 -0400

    added reset, added remove()

--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@ document.addEventListener("DOMContentLoaded", () => {
   createTask.addEventListener('click', function(e) {
     e.preventDefault()
     console.dir(e.target)
+
     const liContent = document.getElementById("new-task-description").value 
-    console.log(liContent)
+    // console.log(liContent)
+
     const liElement = document.createElement("li")
     const ulElement = document.getElementById("tasks")
-    liElement.innerHTML = `${liContent} <button class="delete-task"> X </button>`
-    console.log(liElement)
+    liElement.innerText = liContent
+
+    let buttonTag = document.createElement("button")
+    // console.log(buttonTag)
+    // console.log(liElement)
+
+    buttonTag.innerText = "X"
+    liElement.appendChild(buttonTag)
     ulElement.appendChild(liElement)
-    let deleteTask = document.getElementsByClassName("delete-task")
-    console.log(deleteTask)
+
+    e.target.parentElement.reset()
+    
+    buttonTag.addEventListener('click', function(e){
+        e.target.parentElement.remove()
+    })
+
   })
 
 });
