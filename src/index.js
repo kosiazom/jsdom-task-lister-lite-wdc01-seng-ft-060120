68168ef1a5697a0e53fa6c140ba98743c621d620
andy-aguilar <aaguil3@gmail.com>
2020-07-14 16:43:49 -0400

    created li and appended li

--- src/index.js
+++ src/index.js
@@ -3,18 +3,29 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(createTask)
 
    createTask.addEventListener('click', function(e) {
-       e.preventDefault()
-       console.dir(e.target)
+    e.preventDefault()
+    console.dir(e.target)
 
-     const liContent = document.getElementById("new-task-description").value 
-     console.log(liContent)
+    const liContent = document.getElementById("new-task-description").value 
+    console.log(liContent)
+    const liElement = document.createElement("li")
+    const ulElement = document.getElementById("tasks")
+    liElement.innerHTML = `${liContent}`
+    console.log(liElement)
+    ulElement.appendChild(liElement)
+
+    
+
    })
   
 
 
 });
 
-//step 1 get the element pert- createNewTask
-//step 2 create a listener for it, when you click on "create new task"
+//step 1 get the element pert- createNewTask ****DONE****
+//step 2 create a listener for it, when you click on "create new task" ****DONE****
 // step 3 create a function that does alot of things, grab whats in the text box and creates an 'li' in the myTodos
+  // Create an HTML Element (li) *** DONE ***
+  // put liContent inside the li element we created
+  // append that li to my todos
 //step 4 create a delete button with its own ......
